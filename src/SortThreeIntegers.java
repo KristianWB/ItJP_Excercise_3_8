50d0d18306ab1d01dc38a4326996b02308d2eed3
KristianWB <42993999+KristianWB@users.noreply.github.com>
2018-09-27 07:11:16 +0200

    Initial commit

---
+++ src/SortThreeIntegers.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class SortThreeIntegers {
+    public static void main(String[] args)   {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter 3 digits");
+        System.out.print("Enter first digit: ");
+        int number1 = input.nextInt();
+        System.out.print("Enter the second digit: ");
+        int number2 = input.nextInt();
+        System.out.print("Now enter the final digit: ");
+        int number3 = input.nextInt();
+
+        if (number1 >= number2 && number2 >= number3)
+            System.out.println(
+                    number1 + " " + number2 + " " + number3
+                    );
+        else if (number1 >= number3 && number3 > number2)
+            System.out.println(
+                    number1 + " " + number3 + " " + number2
+            );
+        else if (number2 >= number3 && number1 >= number3)
+            System.out.println(
+                    number2 + " " + number1 + " " + number3
+            );
+        else if (number2 >= number3 && number3 > number1)
+            System.out.println(
+                    number2 + " " + number3 + " " + number1
+            );
+        else if (number3 > number1 && number1 >= number2)
+            System.out.println(
+                    number3 + " " + number1 + " " + number2
+            );
+        else if (number3 > number2 && number2 > number1)
+            System.out.println(
+                    number3 + " " + number2 + " " + number1
+            );
+
+    }
+}
